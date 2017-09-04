6cf016b83b4cadb9edce3113cee5e10a087786b6
Coral <coral00685@gmail.com>
2017-09-04 13:16:24 +0800

    BLE TEST

---
+++ app/src/main/java/com/aaronlife/babybeacon/BleDefine.java
@@ -0,0 +1,17 @@
+package com.aaronlife.babybeacon;
+
+public class BleDefine
+{
+    public static final String SERVICE_DEVICE_INFORMATION =
+                                        "00002A26-0000-1000-8000-00805F9B34FB";
+    public static final String DEVICE_NAME =
+                                        "00002A00-0000-1000-8000-00805F9B34FB";
+    public static final String CUSTOM_SERVICE =
+                                        "6E400001-B5A3-F393-E0A9-E50E24DCCA9E";
+    public static final String CHAR_READ =
+                                        "0000FFF1-0000-1000-8000-00805F9B34FB";
+    public static final String CHAR_WRITE =
+                                        "0000FFF2-0000-1000-8000-00805F9B34FB";
+    public static final String CHAR_NOTIFY =
+                                        "0000FFF3-0000-1000-8000-00805F9B34FB";
+}
